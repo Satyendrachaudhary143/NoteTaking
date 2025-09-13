add85ce99ab218b8a58c5346d0f5d9b5fea3da3c
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2025-09-13 19:21:34 +0530

    debug 1921

--- frontend/src/componenets/api/url.js
+++ frontend/src/componenets/api/url.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const baseURL = axios.create({
-    baseURL: "http://localhost:4001/api/v1/notes/",
+    baseURL: "https://note-taking-orpin.vercel.app/api/v1/notes/",
     withCredentials: true
 
 })
