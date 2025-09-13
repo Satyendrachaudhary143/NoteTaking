062dbb52b23c1591a4c96d557e7dc095057f12a7
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2025-09-13 12:07:42 +0530

    debug...

--- frontend/src/componenets/api/url.js
+++ frontend/src/componenets/api/url.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const baseURL = axios.create({
-    baseURL: "http://localhost:4001/api/v1/notes/"
+    baseURL: "https://note-taking-orpin.vercel.app/api/v1/notes/"
 })
 export default baseURL;
--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 // middleware
-app.use(cors())
+app.use(cors({
+   origin: 'http://localhost:5173', // ya production domain
+  credentials: true
+
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // routes
