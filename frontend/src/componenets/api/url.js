7455a9b9db5622e4467c5aa34b30b9f7f6b9f623
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2025-09-14 08:16:35 +0530

    debug 816

--- frontend/src/componenets/api/url.js
+++ frontend/src/componenets/api/url.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const baseURL = axios.create({
-    baseURL: "https://notetaking-xggl.onrender.com/api/v1/notes/",
+    baseURL: "/api/v1/notes/",
     withCredentials: true
 
 })
