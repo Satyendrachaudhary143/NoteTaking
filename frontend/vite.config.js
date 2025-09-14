e6b27aa5a7264e30f970807015e08e39426be7ca
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2025-09-14 09:11:20 +0530

    debug 911

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://notetaking-xggl.onrender.com', // https://note-taking-orpin.vercel.app , http://localhost:4001
-        changeOrigin: true,
-        secure:true
-      } 
-    },
-  },
   plugins: [react(),tailwindcss(),],
 })
